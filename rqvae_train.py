e487d0950a018fb4e2bf8217467bf53f40d9af6e
SerkanAnar <serkan.anar24@gmail.com>
2025-10-19 13:48:22 +0200

    add skeleton code for training

---
+++ rqvae_train.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn.functional as F
+from rqvae import RQVAE, rqvae_loss
+import numpy as np
+
+def train_rqvae(embeddings):
+    # Handle code for dataset here, probably use dataloader etc? 
+
+    # handle training loop here
+    pass
+
+if __name__ == '__main__':
+    # get embeddings
+    # train model
+    # save trained model?
