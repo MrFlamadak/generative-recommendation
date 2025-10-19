aa3013504deb05c007317d45eb892a134972013b
Roy Liu <113428371+ruireng@users.noreply.github.com>
2025-10-19 11:51:57 +0200

    Init skeleton code for Encoder+Decoder
    Signed-off-by: Roy Liu <113428371+ruireng@users.noreply.github.com>

---
+++ vae.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.nn.function as F
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim=784, latent_dim=32, hidden_dim=256):
+        super().__init__()
+        
+        pass
+    
+    
+    def forward(self, x):
+        pass
+    
+    
+class Decoder(nn.Module):
+    def __init__(self, latent_dim=32, output_dim=784, hidden_dim=256):
+        super().__init__()
+        
+        pass
+    
+    
+    def forward(self, x):
+        pass
