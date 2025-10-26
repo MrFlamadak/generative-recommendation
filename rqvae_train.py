a49216efc03375d3f8e89c7ba978ad7be3a374c8
SerkanAnar <serkan.anar24@gmail.com>
2025-10-26 12:30:09 +0100

    add sanity check and training loop

--- rqvae_train.py
+++ rqvae_train.py
@@ -1,53 +1,95 @@
 import torch
+import torch.optim as optim
+import numpy as np
+from torch.utils.data import DataLoader, TensorDataset
 import torch.nn.functional as F
-from rqvae import RQVAE, rqvae_loss
 
-def train_rqvae(embeddings):
-    # Handle code for dataset here, probably use dataloader etc? 
+def rqvae_loss(x, x_recon, commitment_loss, beta=0.25):
+    recon_loss = F.mse_loss(x_recon, x, reduction='mean')
+    c_loss = commitment_loss.mean()
+    total_loss = recon_loss + beta * c_loss
+    return total_loss
 
-    # handle training loop here
-    num_epochs = 100
-    model = RQVAE(input_dim=embeddings.shape[1],
-                  latent_dim=32,
-                  hidden_dim=256,
-                  codebook_size=512,
-                  num_quantizers=4)
+def train_rqvae_sanity_check(rqvae, embeddings, n_samples=100, epochs=5, batch_size=64, lr=1e-3, verbose=True):
+    test_embeddings = embeddings[:n_samples].clone()
+    if verbose:
+        print(f"Testing with {n_samples} samples.")
     
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-    model.train()
-    for epoch in range(num_epochs):
-        total_loss = 0
-        total_recon_loss = 0
-        total_commitment_loss = 0
-        for _, (batch_embeddings, ) in enumerate(dataloader):
+    dataset = TensorDataset(test_embeddings)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    optimizer = optim.Adam(rqvae.parameters(), lr=lr)
+    rqvae.train()
+    losses = []
 
-            x_recon, semantic_ids, commitment_loss = model(batch_embeddings)
-
-            loss = rqvae_loss(batch_embeddings, x_recon, commitment_loss)
-
+    for epoch in range(epochs):
+        epoch_losses = []
+        for batch_idx, (batch_data,) in enumerate(dataloader):
             optimizer.zero_grad()
+            x_recon, _, commitment_loss = rqvae(batch_data)
+            loss = rqvae_loss(batch_data, x_recon, commitment_loss)
             loss.backward()
             optimizer.step()
+            epoch_losses.append(loss.item())
+        avg_loss = np.mean(epoch_losses)
+        losses.append(avg_loss)
 
-            total_loss += loss.item()
-            recon_loss = F.mse_loss(x_recon, batch_embeddings, reduction="mean")
-            total_recon_loss += recon_loss.item()
-            total_commitment_loss += commitment_loss.mean().item()
-
-        if (epoch+1) % 10 == 0:
-            average_loss = total_loss / len(dataloader)
-            average_recon_loss = total_recon_loss / len(dataloader)
-            average_commitment_loss = total_commitment_loss / len(dataloader)
-
-            print(f'Epoch {epoch+1}/{num_epochs}:')
-            print(f'Total loss: {average_loss:.4f}')
-            print(f'Reconstruction loss: {average_recon_loss:.4f}')
-            print(f'Commitment loss: {average_commitment_loss:.4f}')
+        if verbose:
+            print(f"Epoch {epoch+1} average loss: {avg_loss:.4f}")
     
-    return model
+    rqvae.eval()
+    return rqvae
 
-if __name__ == '__main__':
-    # get embeddings
-    # train model
-    # save trained model?
-    pass
+def train_rqvae_full(rqvae, embeddings, epochs=100, batch_size=128, lr=1e-3, save_path=None, checkpoint_freq=10, verbose=True):
+    dataset = TensorDataset(embeddings)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    optimizer = optim.Adam(rqvae.parameters(), lr=lr)
+
+    rqvae.train()
+    losses = []
+    best_loss = float('inf')
+    
+    for epoch in range(epochs):
+        epoch_losses = []
+        for batch_idx, (batch_data,) in enumerate(dataloader):
+            optimizer.zero_grad()
+            x_recon, _, commitment_loss = rqvae(batch_data)
+            loss = rqvae_loss(batch_data, x_recon, commitment_loss)
+            loss.backward()
+            optimizer.step()
+            epoch_losses.append(loss.item())
+        avg_loss = np.mean(epoch_losses)
+        losses.append(avg_loss)
+
+        if verbose and (epoch+1) % 10 == 0:
+            print(f'Epoch {epoch+1}/{epochs}, loss = {avg_loss:.4f}')
+
+        if save_path and (epoch+1) % checkpoint_freq == 0:
+            checkpoint_path = f'{save_path}_epoch_{epoch+1}.pth'
+            torch.save({
+                'epoch': epoch+1,
+                'model_state_dict': rqvae.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': avg_loss,
+            }, checkpoint_path)
+        
+        if verbose:
+            print(f'saved checkpoint at epoch {epoch+1}')
+
+        if avg_loss < best_loss:
+            best_loss = avg_loss
+            if save_path:
+                best_path = f'{save_path}_best.pth'
+                torch.save(rqvae.state_dict(), best_path)
+
+    # save not only best model so far, but also the final model
+    if save_path:
+        final_path = f'{save_path}_final.pth'
+        torch.save(rqvae.state_dict(), final_path)
+
+    rqvae.eval()
+    return rqvae
+
+def load_trained_rqvae(rqvae, model_path):
+    rqvae.load_state_dict(torch.load(model_path))
+    rqvae.eval()
+    return rqvae
